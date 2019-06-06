13624547b0ff63c40b4d30b081d0cb222efc1f89
Utsav Vatsa <utvatsa@gmail.com>
2019-06-07 05:10:35 +0530

    project complete

--- js/main.js
+++ js/main.js
@@ -1,20 +1,95 @@
-const maindiv = document.getElementById('maindiv');
+const inputText = document.getElementById('inputText');
+const listOfMatches = document.getElementById('list-of-matches');
 
-const btnSubmit = document.getElementById('btnChange');
 
-btnSubmit.addEventListener('click', function(){
-    console.log("hello world!");
-    maindiv.innerHTML ="hello world!";
-});
 
+//using arrow function that is triggered on event 'input' inside inputText - input element in HTML
+inputText.addEventListener('input', ()=> findStates(inputText.value));
+
+
+// function to parse through states.json and filter as per input
+const findStates = async searchText => {
+    
+    // fetch returns a promise and hence needs to be used in an async function with an await call
+    //.json also returns a promise and needs to be used with await
+    const fetched= await fetch('../data/states.json');
+    const allStates = await fetched.json();
+    //console.log(allStates);
+
+    //using regular expression to find match of states with the input text
+    const expression = new RegExp(`^${searchText}`, 'gi');
+
+    //using filter - higher order array method like map - takes a function - loops through the array(here json array) and returns elements based on condition put in function
+    let matches = allStates.filter(function(state){
+        return state.name.match(expression) || state.abbr.match(expression);
+    });
+    
+    //makes matches as null when text in input field is cleared out
+    if(searchText.length === 0){
+        matches = [];
+        listOfMatches.innerHTML = "";
+    }
+
+    //console.log(matches);
+    displayList(matches);
+     
+}
+
+// function to display output on HTML
+
+const displayList = matches => {
+    if(matches.length > 0){
+        
+        //map is a higher order array function. returns an array from an array
+        //here returns an array of HTML as a div for each matched element
+
+        const HTMLtext = matches.map(function(match){
+            const html = `<div class="card card-body mb-4">
+                <h4>
+                    ${match.name} (${match.abbr}) <span class="text-primary"> ${match.capital} </span>
+                </h4>
+                <small>
+                    LAT: ${match.lat} / LONG: ${match.long}
+                </small>
+            </div>`;
+
+            return html;
+        });
+        //console.log(HTMLtext);
+
+        //join to make a single string of HTML fromn the array of HTML.
+
+        HTMLmatchlist = HTMLtext.join('');
+        //console.log(HTMLmatchlist);
+
+        //putthing HTML in list-of-matches
+        listOfMatches.innerHTML = HTMLmatchlist;
+
+    }
+
+   
+}
+
+
+//using array function for map!
 /*
-//use any one
-btnSubmit.addEventListener('click', () => changesome(maindiv));
+const displayList = matches => {
+    if(matches.length > 0){
+        const HTMLtext = matches.map(
+            match=>`<div class="card card-body mb-4">
+                <h4>
+                    ${match.name} (${match.abbr}) <span class="text-primary"> ${match.capital} </span>
+                </h4>
+                <small>
+                    LAT: ${match.lat} / LONG: ${match.long}
+                </small>
+                </div>`
+        ).join('');
 
+       // console.log(HTMLtext);
 
-const changesome = dividiv => {
-    console.log("hi world");
-    dividiv.innerHTML = "hi world";
-};
+        listOfMatches.innerHTML = HTMLtext;
+    }
 
+}
 */
